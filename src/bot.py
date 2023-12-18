1f43beb99baa298e9450dabde045969f1ee2e86b
angr1it <andgr1it@gmail.com>
2023-12-18 13:22:58 +0300

    backend outline; inference example added;

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,9 @@
 import os
+
 from dotenv import load_dotenv
 import telebot
+
+from inference.model_inference import process
 
 load_dotenv()
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
@@ -8,15 +11,25 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-@bot.message_handler(commands=["start", "hello"])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+@bot.message_handler(commands=["help"])
+def help(message):
+    bot.reply_to(
+        message,
+        "/process -- ввод данных для расчета;",
+    )
 
 
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=["process"])
+def process_handler(message):
+    send = bot.send_message(
+        message.chat.id, "Введите переменные в формате key: value через запятую.\nНапример:\n a: 1, b: 2"
+    )
+    bot.register_next_step_handler(send, process_following)
 
 
-print("Starting bot...")
+def process_following(message):
+    bot.send_message(message.chat.id, process(message.text))
+
+
+print("Bot started")
 bot.infinity_polling()
---
+++ src/inference/errors.py
@@ -0,0 +1,10 @@
+class InternalError(Exception):
+    pass
+
+
+class ParseError(Exception):
+    pass
+
+
+class WrongParamsError(Exception):
+    pass
